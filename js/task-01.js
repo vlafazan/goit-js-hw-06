f5190adcd4434b28e9fde9d43bcc61558467cd5d
Vladyslav Lafazan <89795008+vlafazan@users.noreply.github.com>
2022-08-10 19:25:34 +0300

    task1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const totalCategories = document.querySelectorAll('.item')
+console.log(`Number of categories: ${totalCategories.length}`);
+
+totalCategories.forEach(item => {
+console.log(`Category: ${item.firstElementChild.textContent}`);
+console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
+console.log(totalCategories);
